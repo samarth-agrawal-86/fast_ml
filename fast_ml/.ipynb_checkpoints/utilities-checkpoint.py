241c88df6e1287f411861c55a2c9f27dcb609867
Samarth <sensational.sam18@gmail.comgit config --listgitconfiggit config --global user.email sensational.sam18@gmail.com>
2020-07-13 21:39:51 +0530

    Initial commit

---
+++ fast_ml/.ipynb_checkpoints/utilities-checkpoint.py
@@ -0,0 +1,115 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+import seaborn as sns
+from IPython.display import Markdown, display
+
+def __printmd__(string):
+    display(Markdown(string))
+
+    
+def __normality_diagnostic__ ( s):
+    plt.figure(figsize = (16, 4))
+
+    plt.subplot(1,2,1)
+    sns.distplot(s, hist = True, fit = norm, kde = True)
+    plt.title('Histogram')
+
+    plt.subplot(1,2,2)
+    stats.probplot(s, dist="norm", plot=plt)
+    plt.ylabel('RM Quantiles')
+
+    plt.show()
+
+
+def __missing_rare_category__ (df, c, add_missing, add_rare, tol=0.05):
+    length_df = len(df)
+    
+    if add_missing:
+        df[c] = df[c].fillna('Missing')
+    
+    s = pd.Series(df[c].value_counts() / length_df)
+    s.sort_values(ascending = False, inplace = True)
+    
+    if add_rare:
+        non_rare_label = [ix for ix, perc in s.items() if perc>tol]
+        df[c] = np.where(df[c].isin(non_rare_label), df[c], 'Rare')
+
+    return df
+        
+def __plot_categories__( df, c,  add_missing = False, add_rare = False, tol=0.05):
+
+    length_df = len(df)
+    
+    df = __missing_rare_category__(df, c, add_missing, add_rare, tol=0.05)
+
+    plot_df = pd.Series(df[c].value_counts() / length_df)
+    plot_df.sort_values(ascending = False, inplace = True)
+
+
+    fig = plt.figure(figsize=(12,4))
+    ax = plot_df.plot.bar(color = 'royalblue')
+    ax.set_xlabel(c)
+    ax.set_ylabel('Percentage')
+    ax.axhline(y=0.05, color = 'red')
+    plt.show()
+
+    
+def __plot_categories_with_target__( df, c, target):
+
+    plot_df = __calculate_mean_target_per_category__(df, c, target)
+    #plot_df.reset_index(drop = True, inplace=True)
+
+
+    fig, ax = plt.subplots(figsize=(12,4))
+    plt.xticks(plot_df.index, plot_df[c], rotation = 90)
+
+    ax.bar(plot_df.index, plot_df['perc'], align = 'center', color = 'lightgrey')
+
+    ax2 = ax.twinx()
+    ax2.plot(plot_df.index, plot_df[target], color = 'green')
+
+    ax.axhline(y=0.05, color = 'red')
+
+    ax.set_xlabel(c)
+    ax.set_ylabel('Percentage Distribution')
+    ax2.set_ylabel('Mean Target Value')
+
+
+    plt.show()
+
+'''
+def __calculate_mean_target_per_category__(df, c, target):
+    
+    length_df = len(df)
+    temp = pd.DataFrame(df[c].value_counts()/length_df)
+    temp = pd.concat([temp, pd.DataFrame(df.groupby(c)[target].mean())], axis=1)
+    temp.columns = ['perc', target]
+    temp.reset_index(inplace=True)
+    temp.sort_values(by='perc', ascending = False, inplace=True)
+    return temp
+'''
+
+def __calculate_mean_target_per_category__(df, c, target):
+    
+    length_df = len(df)
+    temp = pd.DataFrame(df[c].value_counts()/length_df)
+    temp = pd.concat([temp, pd.DataFrame(df.groupby(c)[target].mean())], axis=1)
+
+    temp.reset_index(inplace=True)
+    temp.columns = [c, 'perc', target]
+    temp.sort_values(by='perc', ascending = False, inplace=True)
+
+    return temp
+
+def __plot_target_with_categories__(df, c, target):
+    
+    fig = plt.figure(figsize=(12,6))
+    for cat in df[c].unique():
+        df[df[c]==cat][target].plot(kind = 'kde', label = cat)
+
+    plt.xlabel(f'Distribution of {target}')
+    plt.legend(loc='best')
+    plt.show()
---
+++ fast_ml/missing_data_analysis.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from IPython.display import Markdown, display
+from fast_ml.utilities import __printmd__ , __normality_diagnostic__ , __plot_categories__ , \
+__plot_categories_with_target__ , __calculate_mean_target_per_category__ , __plot_target_with_categories__
+
+class MissingDataAnalysis:
+    def __init__ (self, df, target = None, model = None):
+        self.__df__ = df
+        self.__target__ = target
+        self.__model__ = model
+        
+    # ------------------------------------------------------#
+    # Numerical Variable  #
+    # ------------------------------------------------------#
+    
+    def explore_numerical_imputation (self, variable):
+        
+        return None
+    
+    # ------------------------------------------------------#
+    # Categorical Variable #
+    # ------------------------------------------------------#
+    
+        
+    def explore_categorical_imputation (self, variable):
+        """
+        Compares the results from various imputation methods so that you can choose the best suited one
+
+        
+        # 1st chart => existing categories and avg target value
+        # 2nd chart => missing value replaced by frequent category ; then plot a chart with target value
+        # 3rd chart => missing value replaced by 'Missing' category ; then plot a chart with target value
+        # 4th chart => missing value replaced by random distribution ; then plot a chart with target value
+        
+        """
+        df = self.__df__
+        c = variable
+        
+        __printmd__ ('**<u>Missing Values :</u>**')
+
+        print ('  Number :', df[c].isnull().sum())
+        print ('  Percentage :', df[c].isnull().mean()*100, '%')
+        print ()
+        
+        __printmd__(f'**<u>We have following options for Imputing the Missing Value for Categorical Variable, {c} :</u>**')
+        print ('  1. Imputing missing values by Frequent Category' )
+        print ('  2. Imputing missing values by Missing Label' )
+        print ('  3. Imputing missing values by Randomly selected value' )
+        
+        print ()
+        print ("Let's visualize the impact of each imputation and compare it with original distribution")
+        print ()
+        
+        __printmd__ ('**<u>1. Original Distribution of all Categories :</u>**')
+        __plot_categories_with_target__(df, c, target = self.__target__)
+        
+        __printmd__ ('**<u>2. All Categories after Frequent Category Imputation :</u>**')
+        
+        # Frequent value
+        print ('Look at the Distibution of Frequent Category and Missing Data. Are there some major differences')
+        fig = plt.figure(figsize = (8,4))
+        ax = fig.add_subplot(111)
+        
+        value = df[c].mode().item()
+        print ('\n\nMost Frequent Category: ', value)
+        
+        df[df[c] == value][self.__target__].plot(kind = 'kde', ax = ax, color = 'blue')
+        
+        # NA Value
+        df[df[c].isnull()][self.__target__].plot(kind = 'kde', ax = ax, color = 'red')
+        
+        # Add the legend
+        labels = ['Most Frequent category', 'with NA']
+        ax.legend(labels, loc = 'best')
+        plt.show()
+        
+        
+        df[c+'_freq'] = df[c].fillna(value)
+        
+        __plot_categories_with_target__(df, c+'_freq', target = self.__target__)
+        
+        
+        print ("3. All Categories after Missing Label Imputation")
+        value = 'Missing'
+        df[c+'_miss'] = df[c].fillna(value)
+        
+        __plot_categories_with_target__(df, c+'_miss', target = self.__target__)
+        
+        
+        print ("4. All Categories after Randomly Selected Value Imputation")
+        temp = self.__random_category_imputation__(c)
+        __plot_categories_with_target__(temp, c+'_random', target = self.__target__)
+        
+        
+        
+    
+    def __random_category_imputation__(self, c):
+        
+        # Get the number of null values for variable
+        number_nulls = self.__df__[c].isnull().sum()
+        
+        # Get that many number of values from dataset chosen at random
+        random_sample = self.__df__[c].dropna().sample(number_nulls, random_state = 0)
+        
+        # Set the index of random sample to that of null values
+        random_sample.index = self.__df__[self.__df__[c].isnull()].index
+        
+        # make a copy of dataset including NA 
+        self.__df__[c+'_random'] = self.__df__[c].copy()
+        
+        # replace the NA in newly created variable
+        self.__df__.loc[self.__df__[c].isnull(), c+'_random'] = random_sample
+        
+        return self.__df__  
+    
+        
